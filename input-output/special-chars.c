c16b91fd409ca8330b60ea26bfaf9b0f1c30af74
Tom Scott <tomscott85@gmail.com>
2018-03-31 15:12:21 +1100

    count special chars

---
+++ input-output/special-chars.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* count digits, white space, others */
+
+int main()
+{
+	int c, i, nwhite, nother;
+	int ndigit[10];
+
+	nwhite = nother = 0;
+	for (i = 0; i < 10; ++i)
+		ndigit[i] = 0;
+
+	while ((c = getchar()) != 'X')
+		if (c >= '0' && c <= '9')
+			++ndigit[c-'0'];
+		else if (c == ' ' || c == '\n' || c == '\t')
+			++nwhite;
+		else
+			++nother;
+
+	printf("digits: \n");
+	for (i = 0; i < 10; ++i)
+		printf("%d: %d\n", i, ndigit[i]);
+	printf("white space: %d\n", nwhite);
+	printf("other: %d\n", nother);
+}
+
