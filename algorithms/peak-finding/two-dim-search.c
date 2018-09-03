1d11b0d08034673a336a9f5afeef957043fb58dc
Tom Scott <tomscott85@gmail.com>
2018-09-03 21:10:25 +1000

    generate and pass a dynamic 2d array

--- algorithms/peak-finding/two-dim-search.c
+++ algorithms/peak-finding/two-dim-search.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "binary-one-dim.h"
 
-
+void print2d(int ** array, int rows, int cols) {
+  int i, j;
+  for (i=0; i<rows; i++) {
+    for (j=0; j<cols; j++) {
+      printf("%10d ", array[i][j]);
+    }
+    puts("");
+  }
+}
 int * get_one_dim(int two_dim_array[][3], int pos, int is_col) {
+  /* this functions pulls a one dimensional array out of a two dimensional
+  ** array given a row or columns
+  */
+
   static int r[3];
   int i, j;
 
@@ -15,18 +28,26 @@ int * get_one_dim(int two_dim_array[][3], int pos, int is_col) {
 }
 
 int main() {
-  int return_value, *new_one_dim, i;
-  int one_dim_array[] = {4, 5, 9, 10, 2, 15, 14, 10, 10, 10, 10, 6, 8, 16, 15, 13, 17, 13}, array_length;
+  int *new_one_dim, i;
 
   int two_dim_array[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
   new_one_dim = get_one_dim(two_dim_array, 1, 1);
 
   for (i=0; i<3; i++) {
-    printf("%d\n", *(new_one_dim +i));
+    printf("%d\n", *(new_one_dim + i));
   }
 
-  // array_length = sizeof(one_dim_array)/sizeof(int);
-  // printf("start: 0, end: %d\n", array_length);
-  // return_value = binary_search(one_dim_array, 0, array_length);
-  // printf("array position: %d\n", return_value);
+  int **dynamic_two_dim;
+  int num_rows = 5;
+
+  // allocate memory to store pointers to each row
+  dynamic_two_dim = malloc(num_rows*sizeof(int*));
+
+  int x, num_cols = 6;
+  for (x=0; x<num_cols; x++) {
+    // allocate memory to store arrays of col values for each row
+    dynamic_two_dim[x] = malloc(num_cols*sizeof(int));
+  }
+  print2d(dynamic_two_dim, num_rows, num_cols);
+  // for (x=0; x)
 }
