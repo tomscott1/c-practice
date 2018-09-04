640b308dcd2ca06d9335267c4d6215d027bb1a52
Tom Scott <tomscott85@gmail.com>
2018-09-04 21:15:45 +1000

    generate random 2d array and extract array from column m/2

--- algorithms/peak-finding/two-dim-search.c
+++ algorithms/peak-finding/two-dim-search.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "binary-one-dim.h"
 
 void print2d(int ** array, int rows, int cols) {
@@ -11,15 +12,18 @@ void print2d(int ** array, int rows, int cols) {
     puts("");
   }
 }
-int * get_one_dim(int two_dim_array[][3], int pos, int is_col) {
+
+int * get_one_dim(int ** two_dim_array, int pos, int is_col, int num_rows_or_cols) {
   /* this functions pulls a one dimensional array out of a two dimensional
   ** array given a row or columns
   */
 
-  static int r[3];
+  int* r;
+  r = malloc(num_rows_or_cols*sizeof(int));
+
   int i, j;
 
-  for (i=0; i<3; i++) {
+  for (i=0; i<num_rows_or_cols; i++) {
     if (is_col) r[i] = two_dim_array[i][pos];
     if (!is_col) r[i] = two_dim_array[pos][i];
   }
@@ -28,26 +32,43 @@ int * get_one_dim(int two_dim_array[][3], int pos, int is_col) {
 }
 
 int main() {
-  int *new_one_dim, i;
+  int *new_one_dim, **two_dim_array;
+  int num_rows = 5, num_cols = 6;
+  int x, i, j, position;
 
-  int two_dim_array[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-  new_one_dim = get_one_dim(two_dim_array, 1, 1);
+  srand(time(NULL));
 
-  for (i=0; i<3; i++) {
+  // allocate memory to store pointers to each row
+  two_dim_array = malloc(num_rows*sizeof(int*));
+
+  for (x=0; x<num_cols; x++) {
+    // allocate memory to store arrays of col values for each row
+    two_dim_array[x] = malloc(num_cols*sizeof(int));
+  }
+
+  // populate the array with random numbers
+  for (i=0; i<num_rows; i++) {
+    for (j=0; j<num_cols; j++) {
+      two_dim_array[i][j] = (rand() % 20) + 1;
+    }
+  }
+
+  // print the array
+  print2d(two_dim_array, num_rows, num_cols);
+
+  // get the array in column m/2
+  position = (num_cols / 2) - 1;
+  new_one_dim = get_one_dim(two_dim_array, position, 1, num_cols);
+
+  // TODO: perform a binary search on this array
+
+  for (i=0; i<num_rows; i++) {
     printf("%d\n", *(new_one_dim + i));
   }
 
-  int **dynamic_two_dim;
-  int num_rows = 5;
-
-  // allocate memory to store pointers to each row
-  dynamic_two_dim = malloc(num_rows*sizeof(int*));
-
-  int x, num_cols = 6;
+  // de-allocate memory
   for (x=0; x<num_cols; x++) {
-    // allocate memory to store arrays of col values for each row
-    dynamic_two_dim[x] = malloc(num_cols*sizeof(int));
+    free(two_dim_array[x]);
   }
-  print2d(dynamic_two_dim, num_rows, num_cols);
-  // for (x=0; x)
+  free(two_dim_array);
 }
