1f0d6dc4d965c9e23376163c41234e3ab550697a
Tom Scott <tomscott85@gmail.com>
2018-10-03 17:59:24 +1000

    finalise merge sort

--- algorithms/merge-sort/main.c
+++ algorithms/merge-sort/main.c
@@ -26,38 +26,44 @@ void merge(int array[], int left, int right, int length) {
   position1 = left;
   position2 = (( left + right) / 2) + 1;
 
-  while ((position1<((l+r)/2)+1) &&(position2<r+1)) {
+  while ((position1 < ((left + right)/2)+1) && (position2 < right + 1)) {
     if (array[position1] <= array[position2]) {
       array_tmp[current++] = array[position1];
       position1++;
-      printf("position 1 incremented to: %d", position1);
     } else {
       array_tmp[current++] = array[position2];
       position2++;
-      printf("position 2 incremented to: %d", position2);
     }
 
-    if (position2 = right+1) {
-      while ((position1<((left+rright)/2)+1)) {
+    if (position2 == right + 1) {
+      while ((position1<((left+right)/2)+1)) {
         array_tmp[current++] = array[position1];
         position1++;
-        printf("position 1 incremented to: %d", position1);
-      }
+      };
+    } else {
+      while ((position2 < right + 1)) {
+        array_tmp[current++] = array[position2];
+        position2++;
+      };
+    }
+
+    for (current = left; current < right - left + 1;current++) {
+      array[current] = array_tmp[current];
     }
   }
-
-
 }
 
 
 void merge_sort(int array[], int length, int left, int right) {
   if (right - left == 1) {
     if (array[left] > array[right]) {
+      print_array(array, length);
       swap(&array[left], &array[right]);
+    } else {
     }
   }
   else if (left == right)
-  {}
+{}
   else {
     merge_sort(array, length, left, (left + right)/2);
     merge_sort(array, length, ((left + right) / 2) + 1, right);
